30c3cbf9351060ee70be92b465586a42c5eff364
PedroCobucci <107273363+PedroCobucci@users.noreply.github.com>
2024-11-20 23:23:39 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import { ClienteController } from './controllers/cliente.controller'
 import { NotificacaoController } from './controllers/notificacao.controller'
 import { WebHookController } from './controllers/webHook.controller'
-import { kesyaController } from './controllers/kesyaController'
 
 const routes = Router()
 
@@ -13,7 +12,5 @@ routes.post('/cadastrarNotificacao', new NotificacaoController().cadastrar)
 routes.post('/webhook', new WebHookController().execute)
 
 routes.get('/webhook', new WebHookController().execute2)
-
-routes.get('/kesya', new kesyaController().execute)
 
 export default routes
