062b964ed08645e9033865a2a72975eeb42b1757
PedroCobucci <107273363+PedroCobucci@users.noreply.github.com>
2024-11-20 23:21:14 -0300

    Delete src/controllers/kesyaController.ts

--- src/controllers/kesyaController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express'
-import path from 'path'
-export class kesyaController{
-    async execute(req: Request, res: Response){
-        return res.sendFile(path.join(__dirname, '../../teste.html'))
-        // return res.status(200).json({message: "oi amor, to pensando em vc. E morrendo de saudade de te ver :)"})
-    }
-}
